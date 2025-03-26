e28ca1fef7f53c367ac23496aa9500788c733995
Sammie Zhang <101011086+sz6084@users.noreply.github.com>
2025-03-26 16:15:36 +0000

    Updated BJPlayer and added comments to BJBot

--- BJBot.java
+++ BJBot.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class BJBot extends BJPlayer {
   private String[] names = {"Bob", "Rob", "Alice", "Aaron", "Sam", "Eddie", "Rachel", "Mike", "Charlie", "Ellie", "Colin", "Kevin", "Victor", "Robin", "Jean", "Katheryne", "Dan", "Mark", "Richard", "Dana", "Elena", "Joe", "Juan", "Tony", "Ella", "Sammy", "Edward", "Ethan", "Jonathan", "Jason", "Evelyn", "Josie", "Sophia", "Bryan", "Allen", "Alan", "Kim", "Chloe", "Claire", "Jerry", "Aventurine"};
   private boolean opMode = false;
-  public BJBot() {
+  public BJBot() { // >=16 hit >=17 stand
     super("");
     randomName();
-    if (super.getName().equals("Aventurine")) {
+    if (super.getName().equals("Aventurine")) { // see next card?
       opMode = true;
     }
   }
--- BJPlayer.java
+++ BJPlayer.java
@@ -1,11 +1,16 @@
 import java.util.*;
 
 public class BJPlayer extends Player {
-  ArrayList<Card> hand = new ArrayList<Card>(); // add and clear func
+  private ArrayList<Card> hand = new ArrayList<Card>(); // getAction
   
   public BJPlayer(String name) {
     super(name);
   }
-  
 
+  public void add(Card c) {
+    hand.add(c);
+  }
+  public void clear() {
+    hand.clear();
+  }
 }
