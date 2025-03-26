3f049f9d350cc53343b0511f27e58ee020bddde4
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2025-03-26 03:51:21 +0000

    start working on pokerplayer action function

--- PokerGame.java
+++ PokerGame.java
@@ -3,7 +3,6 @@ import java.util.*;
 class PokerGame {
   private PokerDeck p = new PokerDeck();
   private PokerPlayer[] players;
-  private int[] blinds = {0, 1, 2}; // current blinds positions
   private int[] bbSize = {10, 20}; // current blinds size
 
   public PokerGame(PokerPlayer[] players) {
@@ -16,8 +15,8 @@ class PokerGame {
 
   public void preflop() { // code to execute preflop
     Card[][] holeCards = p.deal(players.length);
-    p.addChips(players[blinds[1]].removeChips(bbSize[0]));
-    p.addChips(players[blinds[2]].removeChips(bbSize[1]));
+    p.addChips(players[0].removeChips(bbSize[0]));
+    p.addChips(players[1].removeChips(bbSize[1]));
     for (int i = 0; i < holeCards.length; i++) {
       players[i].setHand(holeCards[i]);
       // do some stuff based on action
@@ -46,24 +45,13 @@ class PokerGame {
 
   public void newHand() {
     p.reset();
+    PokerPlayer first = players[0];
+    for (int i = 1; i < players.length; i++) {
+      players[i-1] = players[i];
+    }
+    players[players.length-1] = first;
     for (int i = 0; i < players.length; i++) {
       players[i].setInHand(true);
     }
-    players[blinds[0]].setStatus(0);
-    players[blinds[1]].setStatus(0);
-    players[blinds[2]].setStatus(0);
-    for (int i = 0; i < 3; i++) {
-      if (blinds[i] == players.length - 1) {
-        blinds[i] = 0;
-        if (i == 0) {
-          bbSize[0] *= 2;
-          bbSize[1] *= 2;
-        }
-      }
-      else blinds[i]++;
-    }
-    players[blinds[0]].setStatus(1);
-    players[blinds[1]].setStatus(2);
-    players[blinds[2]].setStatus(3);
   }
 }
--- PokerPlayer.java
+++ PokerPlayer.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class PokerPlayer extends Player {
   Card[] hand;
-  int status; // 0 - regular
+  int status; // -1 regular, 0 dealer, 1 sb, 2 bb
+  Scanner s = new Scanner(System.in);
   public PokerPlayer(String name) {
     super(name);
     hand = new Card[2];
-    status = 0;
+    status = -1;
   }
+
   public Card[] getHand() {
     return hand;
   }
@@ -22,5 +24,20 @@ public class PokerPlayer extends Player {
 
   public void setStatus(int i) {
     status = i;
+  }
+
+  public Object[] action(String round, int bet, int blind) {
+    Object[] action = new Object[2];
+    switch(round) {
+      case "preflop":
+        break;
+      case "flop":
+        break;
+      case "turn":
+        break;
+      case "river":
+        break;
+    }
+    return null;
   }
 }
