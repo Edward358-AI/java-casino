28d327d0648c0967bd5b6804f4b91ff9e83af01e
Sammie Zhang <101011086+sz6084@users.noreply.github.com>
2025-03-26 03:23:30 +0000

    Filled out Blackjack Class

--- BJPlayer.java
+++ BJPlayer.java
@@ -1,9 +1,11 @@
 import java.util.*;
 
 public class BJPlayer extends Player {
-  ArrayList<Card> hand = new ArrayList<Card>();
+  ArrayList<Card> hand = new ArrayList<Card>(); // add and clear func
   
   public BJPlayer(String name) {
     super(name);
   }
+  
+
 }
