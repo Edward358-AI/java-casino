53372baf209d58459e6eb729402de5011983ef88
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2025-05-02 15:53:06 +0000

    remove some unused stuff in bjbot.java

--- BJBot.java
+++ BJBot.java
@@ -1,17 +1,13 @@
-import java.util.*;
+// import java.util.*;
 
 public class BJBot extends BJPlayer {
   private static String[] names = { "Bob", "Rob", "Alice", "Aaron", "Sam", "Eddie", "Rachel", "Mike", "Charlie",
       "Ellie", "Colin", "Kevin", "Victor", "Robin", "Jean", "Katheryne", "Dan", "Mark", "Richard", "Dana", "Elena",
       "Joe", "Juan", "Tony", "Ella", "Sammy", "Edward", "Ethan", "Jonathan", "Jason", "Evelyn", "Josie", "Sophia",
       "Bryan", "Allen", "Alan", "Kim", "Chloe", "Claire", "Jerry", "Aventurine" };
-  private boolean opMode = false;
 
   public BJBot() { // <=16 hit >17 stand
     super(names[(int) (Math.random() * names.length)]);
-    if (super.getName().equals("Aventurine")) { // see next card?
-      opMode = true;
-    }
   }
 
   public int[] action(int gS) {
